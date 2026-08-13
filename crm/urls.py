d4749dc6ef08a1abcf7917789c52ccfa0210dc47
AliAshrafi99 <Ali.Ashrafi99@yahoo.com>
2026-08-13 12:45:01 +0400

    final

--- crm/urls.py
+++ crm/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('dashboard/super-admin/',views.super_admin_dashboard,name='super_admin_dashboard' ),
-    path('dashboard/manager/',views.manager_dashboard,name='manager_dashboard' ),
-    path('dashboard/employee/',views.employee_dashboard,name='employee_dashboard'),
-    path('logout/',views.logout_user,name='logout'),
+    path('dashboard/super-admin/', views.super_admin_dashboard,
+         name='super_admin_dashboard'),
+    path('dashboard/manager/', views.manager_dashboard, name='manager_dashboard'),
+    path('dashboard/employee/', views.employee_dashboard,
+         name='employee_dashboard'),
+    path('logout/', views.logout_user, name='logout'),
 
 
 
